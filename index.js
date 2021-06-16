2dfbe43c8d59ff58bc56974aa231096a4e4a31d2
Michael Kret <dirtysculptor@gmail.com>
2021-06-16 06:15:32 +0300

    mobile layout done

---
+++ index.js
@@ -0,0 +1,5 @@
+document.querySelector("#menu-mobile").addEventListener("click", () => {
+  document.querySelector("#menu-hamburger").classList.toggle("hidden");
+  document.querySelector("#menu-close").classList.toggle("hidden");
+  document.querySelector("#menu-links").classList.toggle("hidden");
+});
